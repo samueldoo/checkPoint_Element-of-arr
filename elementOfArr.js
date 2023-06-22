47a41b184d36546ff3328ebf55f00ec3a88055ce
Alademehin Samuel <alademahinsamuel@yahoo.com>
2023-06-22 11:37:40 +0100

    first commit

---
+++ elementOfArr.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+for (var i = 1; i < arr.length; i++) {
+    var key = arr[i];
+    var j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+    arr[j + 1] = arr[j];
+    j--;
+    }
+
+    arr[j + 1] = key;
+}
+
+return arr;
+}
+console.log(insertionSort(arr));
